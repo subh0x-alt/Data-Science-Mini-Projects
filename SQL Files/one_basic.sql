042150a56e3014129e344458bd18700da5476c65
Subhrajit Guchait <62335998+Subhrajit91939@users.noreply.github.com>
2022-07-03 19:36:15 +0530

    Add files via upload

---
+++ SQL Files/one_basic.sql
@@ -0,0 +1,45 @@
+-- SHOW DATABASES;
+-- SELECT DATABASE();
+-- USE photo_store;
+
+-- CREATE TABLE cameras(
+--     model_name VARCHAR(30),
+--     quantity INT
+-- );
+
+-- DESC cameras;
+
+-- TODO: Cameras table done.
+
+-- DROP TABLE cameras;
+
+-- CREATE TABLE canon_cameras(
+--     model_name VARCHAR(30),
+--     quantity INT
+-- );
+
+-- DESC canon_cameras;
+
+-- TODO: Customer query tasks.
+
+-- INSERT INTO canon_cameras(model_name,quantity)
+-- -- Order of filling data is important.
+-- VALUES("70-D", 12),
+-- ("80-D", 19),
+-- ("EOS-R", 25), 
+-- ("EOS-R5", 80), 
+-- ("EOS-R6", 50);
+
+-- SELECT * FROM canon_cameras;
+-- TASK: Customer Enters the store asks you to list all the camera models available.
+-- SELECT model_name FROM canon_cameras;
+
+-- TASK: Customer Enters the store asks :
+-- How many 80-D can you give right now ?
+-- SELECT model_name, quantity FROM canon_cameras 
+-- WHERE (model_name = '80-d');
+
+-- TASK: Customer Enters the store asks :
+-- I want to buy 50 cameras of same model, what can you supply at his moment ?
+SELECT model_name, quantity FROM canon_cameras 
+WHERE (quantity >= 50);
---
+++ SQL Files/two_basic.sql
@@ -0,0 +1,41 @@
+-- Section-3: iNeuron MySQL Course.
+-- SHOW DATABASES;
+
+-- TODO: Create a customer TABLE ot store all the customer DATA.
+-- CREATE TABLE customers( customer_id INT NOT NULL AUTO_INCREMENT, customer_name VARCHAR(30) NOT NULL, email VARCHAR(40) NOT NULL DEFAULT 'No email provided.', amount_spent INT, PRIMARY KEY(customer_id) );
+-- TODO: add some values in customers
+
+-- INSERT INTO customer(customer_name, email, amount_spent) VALUES ( 'subhrajit', 'subhrajit@email.com', 20000 )
+
+-- DROP TABLE customer;
+-- INSERT INTO customers(customer_name , email , amount_spent) VALUES ('hitesh' , 'hitesh@lco.dev' , 35) , 
+-- ('George' , 'geo@lco.dev' , 45) , ('hitesh' , 'hitesh@lco.dev' , 88) , ('lina' , 'lina@gmail.com' , 78) , 
+-- ('jimmy' , 'jimmy@yahoo.co.in' , 54) , ('lina' , 'lina@yahoo.co.in' , 35) , ('hitesh' , 'hitesh@lco.dev' , 56);
+
+-- TODO: can you give the names of all the customers?
+
+-- SELECT customer_name FROM customers;
+
+-- TODO: can you give the email of all the customers?
+-- SELECT email FROM customers;
+
+-- TODO: can you give the purchases of all the customers?
+-- ALIASING KEYWORD: "AS"
+-- SELECT amount_spent AS purchases FROM customers;
+
+-- TODO: Update Jimmy's email id to jimmy'at'yahoo.com
+-- TODO NOTE: Whenever performing an Update operation it is preffered to perform a Select operation.
+
+-- SELECT * FROM customers WHERE customer_name = 'jimmy';
+-- UPDATE customers SET email='jimmy@yahoo.com' WHERE customer_name = 'jimmy';
+
+-- TODO: Lina's purchase amount needs to be changed to 38 from 35.
+
+-- SELECT * FROM customers WHERE ((customer_name='lina') & (amount_spent=35));
+-- UPDATE customers SET amount_spent=38 WHERE ((customer_name='lina') & (amount_spent=35));
+
+-- TODO: Change all the email ids for Lina to lina'at'gmail.com
+
+-- UPDATE customers
+-- SET email = 'lina@gmail.com'
+-- WHERE customer_name = 'lina';
