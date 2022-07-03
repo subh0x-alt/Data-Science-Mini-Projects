87d9903b817e0907dd296dc87765ecb2e0e8337a
Subhrajit Guchait <62335998+Subhrajit91939@users.noreply.github.com>
2022-07-03 19:40:07 +0530

    Update three_basic.sql

--- SQL Files/three_basic.sql
+++ SQL Files/three_basic.sql
@@ -1,9 +1,9 @@
--- SELECT * FROM customers WHERE customer_name='george';
+SELECT * FROM customers WHERE customer_name='george';
 
 -- TODO: All the George's information from the table.
--- DELETE FROM customers WHERE customer_name='george';
+DELETE FROM customers WHERE customer_name='george';
 
 -- TODO: DELETE all the hitesh names from the customer table.
--- DELETE FROM customers WHERE customer_name='hitesh'
+DELETE FROM customers WHERE customer_name='hitesh'
 
--- DELETE FROM customers WHERE customer_name='lina'
+DELETE FROM customers WHERE customer_name='lina'
