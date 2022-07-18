592370359ae12dff1bd5a4df820fcfd6e0c5f62f
Subhrajit Guchait <62335998+Subhrajit91939@users.noreply.github.com>
2022-07-18 15:29:05 +0530

    Delete SQL Files directory

--- SQL Files/three_basic.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM customers WHERE customer_name='george';
-
--- TODO: All the George's information from the table.
-DELETE FROM customers WHERE customer_name='george';
-
--- TODO: DELETE all the hitesh names from the customer table.
-DELETE FROM customers WHERE customer_name='hitesh'
-
-DELETE FROM customers WHERE customer_name='lina'
